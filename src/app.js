7bedfb7d3f909d2975b25dbf8d00a48eb57d23b9
jmangcucang <105294809+jmangcucang@users.noreply.github.com>
2022-05-11 13:37:57 +0800

    added new sections

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const hamburgerButton = document.getElementById('hamburger')
 const navList = document.getElementById('nav-list')
-
+const mediaScreen = window.matchMedia('(min-width: 1400px)')
 function toggleButton() {
     navList.classList.toggle('show')
 }
 
 hamburgerButton.addEventListener('click', toggleButton)
+
+mediaScreen.onchange = (e) => {
+    if (e.matches) {
+        $('.tools-container .tools-card').show()
+    } else {
+        $('.tools-container .tools-card').hide()
+        $('.tools-container .tools-card:first-child').show()
+    }
+}
